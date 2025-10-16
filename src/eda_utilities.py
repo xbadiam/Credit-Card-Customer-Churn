8a35740633660b677475106dcef5b50ed8ae9788
Xavier Badia <xavbadia@gmail.com>
2025-10-16 13:45:58 +0200

    Update eda_utilities.py

--- src/eda_utilities.py
+++ src/eda_utilities.py
@@ -1,8 +1,7 @@
 
 
-'''
-Importing libraries
-'''
+%%writefile /content/Credit-Card-Customer-Churn/src/eda_utilities.py
+# Importing libraries
 
 # Data manipulation and visualization.
 import pandas as pd
@@ -18,7 +17,16 @@ import sys
 from warnings import filterwarnings
 filterwarnings('ignore')
 
-def getTotalCountAndLabelFromValueFeatureByGroup(feature):
-  count = data_copy[feature].value_counts()
-  label = data_copy[feature].value_counts().index.tolist()
-  return count, label
+
+def getTotalCountAndLabelFromValueFeatureByGroup(data_copy, feature):
+    """
+    Devuelve el recuento y las etiquetas únicas de una característica dada.
+    Args:
+        data_copy (pd.DataFrame): Dataset donde buscar los valores.
+        feature (str): Nombre de la columna a analizar.
+    Returns:
+        tuple: (count, label)
+    """
+    count = data_copy[feature].value_counts()
+    label = data_copy[feature].value_counts().index.tolist()
+    return count, label
