10a00d1d5fbcb1bf52df60c93c2c957d4a45ac56
Xavier Badia <xavbadia@gmail.com>
2025-10-16 12:13:34 +0200

    Update eda_utilities.py

--- src/eda_utilities.py
+++ src/eda_utilities.py
@@ -1,1 +1,24 @@
 
+
+'''
+Importing libraries
+'''
+
+# Data manipulation and visualization.
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Debugging.
+from src.exception import CustomException
+import sys
+
+# Warnings.
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+def getTotalCountAndLabelFromValueFeatureByGroup(feature):
+  count = data_copy[feature].value_counts()
+  label = data_copy[feature].value_counts().index.tolist()
+  return count, label
